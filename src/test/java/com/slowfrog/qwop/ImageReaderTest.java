456d2ed32bab3585f5f251815749d5fe61b6186b
Kunal Marwaha <marwahaha@berkeley.edu>
2019-01-05 03:56:35 -0800

    Fix imagereadertest

--- src/test/java/com/slowfrog/qwop/ImageReaderTest.java
+++ src/test/java/com/slowfrog/qwop/ImageReaderTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.InputStream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -14,12 +13,8 @@ public class ImageReaderTest {
 
     @Test
     public void readImages() throws IOException {
-        System.out.println(this.getClass().getResource("/").getPath());
-
-        final InputStream resourceAsStream = this.getClass().getResourceAsStream("src/test/resources/end.png");
-        System.out.println(resourceAsStream);
-//        BufferedImage img = ImageIO.read(this.getClass().getResourceAsStream("./dist_11.2.png"));
-//        float result = ImageReader.readDistance(img);
-//        assertThat(result, equalTo(11.2f));
+        BufferedImage img = ImageIO.read(getClass().getClassLoader().getResourceAsStream("./dist_11.2.png"));
+        float result = ImageReader.readDistance(img);
+        assertThat(result, equalTo(11.2f));
     }
 }
